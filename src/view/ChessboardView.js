94280773de0d042b2e3ff307be499ff94233a035
Mikko Noromaa <mikko@noromaa.fi>
2025-11-21 10:07:17 +0200

    Update ChessboardView.js

--- src/view/ChessboardView.js
+++ src/view/ChessboardView.js
@@ -154,7 +154,7 @@ export class ChessboardView {
         }
         this.svg.setAttribute("width", "100%")
         this.svg.setAttribute("height", "100%")
-        this.state.invokeExtensionPoints(EXTENSION_POINT.boardResized)
+        this.chessboard.state.invokeExtensionPoints(EXTENSION_POINT.boardResized)
     }
 
     redrawBoard() {
